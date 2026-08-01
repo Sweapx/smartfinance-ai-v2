78a6e6676d281563bbebeb0d54ab38b6ad117cf9
unknown <ychristoper9@gmail.com>
2026-08-01 23:58:42 +0700

    Remove startup.sh to rely on Azure startup command

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Startup script for Azure App Service
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run the application
-python -m uvicorn main:app --host 0.0.0.0 --port 8000
